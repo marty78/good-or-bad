f9181c261b075d7013bc80d237dfc8c753366f81
Martin Hals <martinhals@Martins-MacBook-Pro.local>
2016-02-05 20:39:15 -0800

    Improving accuracy

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -14,6 +14,18 @@ def trainClassifier(training_file):
 		document = document.replace(':','')
 		document = document.replace('?','')
 		document = document.replace('!','')
+		document = document.replace("'",'')
+		document = document.replace('-','')
+		document = document.replace('<','')
+		document = document.replace('>','')
+		document = document.replace('(','')
+		document = document.replace(')','')
+		document = document.replace('/',' ')
+		document = document.replace('*','')
+		document = document.replace('<br /><br />', ' ')
+		# document = document.replace('\xc2\x97', ' ')
+		document = document.lower()
+
 		words = document.split()
 
 		if(words[0] == '1'):	#This is a good review
@@ -25,6 +37,7 @@ def trainClassifier(training_file):
 		words.pop(0)
 
 		for word in words:
+			word = stemming(word)
 			if(word in word_count):
 				word_count[word][category] += 1
 			else:
@@ -38,13 +51,8 @@ def trainClassifier(training_file):
 	return num_good, num_bad, word_count
 
 def classify(testing_file, num_good_training, num_bad_trainig, word_count):
-	training_raw = open(testing_file, 'r')
-	raw_data = training_raw.readlines()
-
-	num_classified_good = 0
-	num_classified_bad = 0
-	num_good = 0
-	num_bad = 0
+	testing_raw = open(testing_file, 'r')
+	raw_data = testing_raw.readlines()
 
 	correctly_classified = 0
 	num_documents = 0
@@ -52,20 +60,32 @@ def classify(testing_file, num_good_training, num_bad_trainig, word_count):
 	prior_good = num_good_training/(num_good_training + num_bad_trainig)
 	prior_bad = num_bad_trainig/(num_good_training + num_bad_trainig)
 	counter = 0
+	print word_count
 	for document in raw_data:
 		document = document.replace('.','')
 		document = document.replace(',','')
 		document = document.replace(':','')
 		document = document.replace('?','')
 		document = document.replace('!','')
+		document = document.replace("'",'')
+		document = document.replace('-','')
+		document = document.replace('<','')
+		document = document.replace('>','')
+		document = document.replace('(','')
+		document = document.replace(')','')
+		document = document.replace('/',' ')
+		document = document.replace('<br /><br />', ' ')
+		# document = document.replace('\xc2\x97', ' ')
+		document = document.lower()
+
 		words = document.split()
 
 		if(words[0] == '1'):	#This is a good review
-			num_good += 1
 			category = 1
+			# print "Actual value: GOOD"
 		else:					#This is a bad review
-			num_bad += 1
 			category = 0
+			# print 'Actual value: BAD'
 		words.pop(0)
 
 		sum_good = 0
@@ -73,6 +93,7 @@ def classify(testing_file, num_good_training, num_bad_trainig, word_count):
 
 		for word in words:
 			counter += 1
+			word = stemming(word)
 			# print word_count[word]
 			if(word in word_count):
 				# print counter
@@ -80,12 +101,12 @@ def classify(testing_file, num_good_training, num_bad_trainig, word_count):
 				# print word_count[word]
 
 				if(word_count[word][1] == 0):
-					prob_word_good == 0.0000001
+					prob_word_good == 0.00000001
 				else:
 					prob_word_good = word_count[word][1]/num_good_training
 
 				if(word_count[word][0] == 0):
-					prob_word_bad == 0.0000001
+					prob_word_bad == 0.00000001
 				else:
 					prob_word_bad = word_count[word][0]/num_bad_trainig
 
@@ -96,18 +117,23 @@ def classify(testing_file, num_good_training, num_bad_trainig, word_count):
 		sum_bad += math.log(prior_bad)
 
 		if(sum_good > sum_bad):
-			num_classified_good += 1
 			if(category == 1):
 				correctly_classified += 1
+			# print 'Guess: GOOD\n'
 		else:
-			num_classified_bad += 1
 			if(category == 0):
 				correctly_classified += 1
+			# print 'Guess: BAD\n'
+
 		num_documents += 1
 
 	return correctly_classified, num_documents
 
-
+def stemming(word):
+	if(word[-1] == 's'):
+		if(word[-4:] == 'sses'):
+			word = word[:-2]
+	return word
 
 
 
